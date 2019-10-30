1143ca46b29f478767e3aa7d6f9cdc32398e2a60
Jacksonwsa35 <40011417+Jacksonwsa35@users.noreply.github.com>
2019-10-30 14:28:48 -0300

    Add files via upload

---
+++ ProjectWeb/app/controllers/Start.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Start extends Controller{
+
+	public static void index(){
+		render();
+	}
+	public static void generic(){
+		render();
+	}
+	public static void elements(){
+		render();
+	}
+	
+	
+	
+}
