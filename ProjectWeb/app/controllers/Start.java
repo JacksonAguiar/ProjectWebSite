6cb2aba6149e50a54ed302edb9a916b597e572e3
Rafael Franco <rafaelribeirofranco4@gmail.com>
2019-12-05 13:37:46 -0300

    Adição do summernote e de model de noticias, ainda não responsivo.

--- ProjectWeb/app/controllers/Start.java
+++ ProjectWeb/app/controllers/Start.java
@@ -7,9 +7,6 @@ public class Start extends Controller{
 	public static void index(){
 		render();
 	}
-	public static void generic(){
-		render();
-	}
 	public static void elements(){
 		render();
 	}
